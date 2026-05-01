8652ae04db6a8ec47d109064c717988369a8bd16
doodledoodle128-prog <doodle.doodle128@gmail.com>
2026-05-01 17:09:00 -0400

    Add files via upload

---
+++ password.py
@@ -0,0 +1,7 @@
+while True:
+    code = input("key in password: ")
+    if code == "password123":
+        print("Acsess Granted!")
+        break
+    else:
+        print("Wrong password. Try again")
